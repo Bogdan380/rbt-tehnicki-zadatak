c139720dcbadd7fe5324f0fe87fb331cf1150224
Bogdan Mitrovic <bogdanmitrovic.lp@gmail.com>
2024-07-12 01:43:54 +0200

    Second task finished

--- angular-blog-app/src/app/app-routing.module.ts
+++ angular-blog-app/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { FirstTaskComponent } from './components/first-task/first-task.component';
+import { SecondTaskComponent } from './components/second-task/second-task.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'first-task',
     component: FirstTaskComponent,
+  },
+  {
+    path: 'second-task',
+    component: SecondTaskComponent,
   },
   {
     path: '**',
--- angular-blog-app/src/app/components/second-task/second-task.component.ts
+++ angular-blog-app/src/app/components/second-task/second-task.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-second-task',
   templateUrl: './second-task.component.html',
-  styleUrl: './second-task.component.scss'
+  styleUrl: './second-task.component.scss',
 })
 export class SecondTaskComponent {
-
+  scrollTo(el: HTMLElement) {
+    el.scrollIntoView();
+  }
 }
